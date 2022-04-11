53de3c5648ee9f705436789b870d632341d453e2
jpb <jakub.baranski.33@gmail.com>
2022-04-11 12:00:14 +0100

    Add download functionality to the MyApp UWP LTSC page

--- Views/MyApp_UWP_LTSCPage.xaml.cs
+++ Views/MyApp_UWP_LTSCPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml.Controls;
 
 namespace AppStore.Views
@@ -28,6 +31,34 @@ namespace AppStore.Views
         private void hyperlinkclick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(jpb_AppsPage));
+        }
+
+        private async void download(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            try
+            {
+                var myappuri = new Uri("https://occoam.com/jpb/wp-content/uploads/MyAppUWP1.3.zip");
+                BackgroundDownloader downloader = new BackgroundDownloader();
+                Uri source = myappuri;
+                FolderPicker picker = new FolderPicker { SuggestedStartLocation = PickerLocationId.Downloads };
+                picker.FileTypeFilter.Add("*");
+                StorageFolder folder = await picker.PickSingleFolderAsync();
+                if (folder != null)
+                {
+                    StorageFile testfile = await folder.CreateFileAsync("MyAppUWPLTSC.zip", CreationCollisionOption.ReplaceExisting);
+                    DownloadOperation download = downloader.CreateDownload(source, testfile);
+                    await download.StartAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            new ToastContentBuilder()
+                .AddArgument("action", "viewConversation")
+                .AddArgument("conversationId", 9813)
+                .AddText("MyApp UWP LTSC download complete")
+                .AddText("Take a look!")
+                .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 5, your TFM must be net5.0-windows10.0.17763.0 or greater
         }
 
         private void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
